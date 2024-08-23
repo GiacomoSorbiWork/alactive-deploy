81a0498e30a31e3883afad365b1b0f45963b44ae
GiacomoSorbiWork <giacomo.sorbi.work@gmail.com>
2024-08-23 09:39:29 -0500

    feat: Add video as background

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,8 +2,25 @@ import React from "react";
 import SwipeableEdgeDrawer from "../../components/SwipeableEdgeDrawer";
 
 const DashBoard: React.FC = () => {
+  const url =
+    "https://s3-figma-videos-production-sig.figma.com/video/1267800981591854695/TEAM/08d0/bd09/-14c7-44ca-b923-a3436e290c96?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aMMwUnH5kfSTj56rB5Rp3RjRyLnTSo11ugDGbxe410xllYK5LNQ0wzQKhsgXmsvzU5PGvMST8QEzsxY086~pZcPYMqIkhj0UOKkCK4I1PSH6YW59FI3~OKAFxDrh7H6E5DoCgFw0Dsg4DD~ovArSwsF3JywwyzL-WNrUwfuLhwHYIDC14Y9P3RPXey0Urk1ERbR6gXLrB94JluZZqsjvqGtERIZqPS1vxPpGbQ-C4J58kgmm7qVfiUugqW5jjbPkkXDBFF~KFj1ziiZxfC1tDnJzqiz1V6gTd3cTlD-kI86GEzd9rSbGalJ0qEyxIGBn5C4B7fycA43vK-4KA2sB~A__";
   return (
     <>
+      <div className="relative w-full h-screen overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-1/2 left-1/2 w-full h-full object-cover transform -translate-x-1/2 -translate-y-1/2"
+        >
+          <source src={url} type="video/mp4" />
+        </video>
+        {/* Content that overlays the video */}
+        <div className="relative z-10">
+          {/* Add any content you want on top of the video */}
+        </div>
+      </div>
       <SwipeableEdgeDrawer />
     </>
   );
