fa2d82578e8d88d7ea7fd7c13d47850c1db9ab7c
GiacomoSorbiWork <giacomo.sorbi.work@gmail.com>
2024-08-26 00:23:45 -0500

    fix: Fix Footer bar

--- src/components/FooterBar/index.tsx
+++ src/components/FooterBar/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import classNames from "classnames";
 import FavoriteSVG from "../../../resources/svg/favorite.svg";
+import ActiveFavoriteSVG from "../../../resources/svg/active-favorite.svg";
 import ProfileSVG from "../../../resources/svg/Frame.svg";
 import ActiveHomeSVG from "../../../resources/svg/active-home.svg";
+import HomeSVG from "../../../resources/svg/home.svg";
 
 const FooterBar: React.FC = () => {
+  const location = useLocation();
+
   return (
-    <div className="backdrop-blur-md h-[75px] flex justify-around items-center fixed bottom-0 w-full">
-      <a href="/dashboard" className="flex flex-col items-center">
-        <img src={ActiveHomeSVG} />
+    <div className="backdrop-blur-md h-[75px] flex justify-around items-center fixed bottom-0 w-full z-20 border-t border-white border-opacity-35">
+      <Link to="/dashboard" className="flex flex-col items-center">
+        <img
+          src={location.pathname === "/dashboard" ? ActiveHomeSVG : HomeSVG}
+          alt="Home"
+        />
         <span
           className={classNames({
             "text-activeButton": location.pathname === "/dashboard",
@@ -16,9 +24,14 @@ const FooterBar: React.FC = () => {
         >
           Home
         </span>
-      </a>
-      <a href="/favorite" className="flex flex-col items-center">
-        <img src={FavoriteSVG} />
+      </Link>
+      <Link to="/favorite" className="flex flex-col items-center">
+        <img
+          src={
+            location.pathname === "/favorite" ? ActiveFavoriteSVG : FavoriteSVG
+          }
+          alt="Favorite"
+        />
         <span
           className={classNames({
             "text-activeButton": location.pathname === "/favorite",
@@ -26,9 +39,9 @@ const FooterBar: React.FC = () => {
         >
           Favorite
         </span>
-      </a>
-      <a href="/profile" className="flex flex-col items-center">
-        <img src={ProfileSVG} />
+      </Link>
+      <Link to="/profile" className="flex flex-col items-center">
+        <img src={ProfileSVG} alt="Profile" />
         <span
           className={classNames({
             "text-activeButton": location.pathname === "/profile",
@@ -36,7 +49,7 @@ const FooterBar: React.FC = () => {
         >
           Profile
         </span>
-      </a>
+      </Link>
     </div>
   );
 };
