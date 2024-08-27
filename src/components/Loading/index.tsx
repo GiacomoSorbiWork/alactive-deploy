5d241a49db732dd0135ec1de16f36bf97cbf77ac
GiacomoSorbiWork <giacomo.sorbi.work@gmail.com>
2024-08-26 21:42:27 -0500

    fix: Change the loading bar

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import "./loading.css"; // Import the CSS styles
+import "./loading.css";
 
-const Loading: React.FC = () => {
+const LoadingSpinner: React.FC = () => {
   return (
-    <div className="h-full w-full flex justify-center items-center">
-      <div className="loader">
-        <span className="loader-text">loading</span>
-        <span className="load"></span>
-      </div>
+    <div className="flex justify-center items-center h-full">
+      <svg
+        viewBox="25 25 50 50"
+        className="w-16 h-16"
+        style={{
+          animation: "rotate4 2s linear infinite",
+        }}
+      >
+        <circle
+          r="20"
+          cy="50"
+          cx="50"
+          fill="none"
+          stroke="var(--secondary-color)"
+          strokeWidth="2"
+          strokeDasharray="1, 200"
+          strokeDashoffset="0"
+          strokeLinecap="round"
+          style={{
+            animation: "dash4 1.5s ease-in-out infinite",
+          }}
+        />
+      </svg>
     </div>
   );
 };
 
-export default Loading;
+export default LoadingSpinner;
