346c3c29c6d46daba44c85cbc16eecbb2f7e5e1d
GiacomoSorbiWork <giacomo.sorbi.work@gmail.com>
2024-08-31 10:22:02 +0100

    fix: Fix the issue on video url

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -16,7 +16,7 @@ import FooterBar from "../../components/FooterBar";
 import { IconButtonProps } from "./type";
 
 const VIDEO_URL =
-  "https://s3-figma-videos-production-sig.figma.com/video/1267800981591854695/..."; // Truncated for brevity
+  "https://s3-figma-videos-production-sig.figma.com/video/1267800981591854695/TEAM/08d0/bd09/-14c7-44ca-b923-a3436e290c96?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aMMwUnH5kfSTj56rB5Rp3RjRyLnTSo11ugDGbxe410xllYK5LNQ0wzQKhsgXmsvzU5PGvMST8QEzsxY086~pZcPYMqIkhj0UOKkCK4I1PSH6YW59FI3~OKAFxDrh7H6E5DoCgFw0Dsg4DD~ovArSwsF3JywwyzL-WNrUwfuLhwHYIDC14Y9P3RPXey0Urk1ERbR6gXLrB94JluZZqsjvqGtERIZqPS1vxPpGbQ-C4J58kgmm7qVfiUugqW5jjbPkkXDBFF~KFj1ziiZxfC1tDnJzqiz1V6gTd3cTlD-kI86GEzd9rSbGalJ0qEyxIGBn5C4B7fycA43vK-4KA2sB~A__"; // Truncated for brevity
 
 const IconButton: React.FC<IconButtonProps> = ({ icon, label, onClick }) => (
   <button
