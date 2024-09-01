7a89ba5eb79be0c233e36e3cfd52970c6e69640a
Lancelot Liu <lancylot2004@proton.me>
2024-09-01 12:29:58 +0100

    fix: Adds `audience` and injects authenticated Apollo client.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./global.css";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -24,6 +26,32 @@ const theme = createTheme({
   },
 });
 
+const ApolloProviderWithAuth0 = ({ children }) => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const httpLink = createHttpLink({
+    uri: "http://localhost/graphql",
+  });
+
+  const authLink = setContext(async (_, { headers }) => {
+    const token = await getAccessTokenSilently();
+
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
@@ -31,11 +59,14 @@ root.render(
         domain="alactive-dev.uk.auth0.com"
         clientId="09K9jptGf6apeXpCpoKKTRUEjwfW9RsW"
         authorizationParams={{
-          // redirect_uri: "http://localhost:5173/",
-          redirect_uri: "https://alactive-deploy.vercel.app",
+          redirect_uri: "http://localhost:5173/",
+          audience: "http://v1.api.alactive.app/graphql"
+          // redirect_uri: "https://alactive-deploy.vercel.app",
         }}
       >
-        <App />
+        <ApolloProviderWithAuth0>
+          <App />
+        </ApolloProviderWithAuth0>
       </Auth0Provider>
     </ThemeProvider>
   </React.StrictMode>
