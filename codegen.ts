3f659fe5bf23e18e0d673bf5ebb0e03553dfaf70
Lancelot Liu <lancylot2004@proton.me>
2024-09-03 20:36:27 +0100

    feat: Adds environment files.

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   // schema: 'https://localhost/graphql',
-  schema: 'https://v1.api.alactive.app/graphql',
+  schema: import.meta.env.VITE_GRAPHQL_ENDPOINT,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
