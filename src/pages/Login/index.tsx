d443389206347de808ca1b262dd26a315e28a0d2
GiacomoSorbiWork <giacomo.sorbi.work@gmail.com>
2024-09-03 17:55:11 +0100

    fix: Fix the style on login and progile page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,24 +6,21 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Login: React.FC = () => {
   const { loginWithRedirect } = useAuth0();
-
   return (
     <IonPage className="fullscreen">
       <IonContent scrollY={false} fullscreen>
         <div className="bg-primaryContainer h-full flex flex-col items-center justify-center p-4">
-            <img src={logo} alt="logo" className="h-[180px]" />
+          <img src={logo} alt="logo" className="h-[180px]" />
         </div>
       </IonContent>
-      {isClicked && (
-        <IonFooter className="p-4 mb-4">
-          <LargeDefaultButton
-            text="Continue"
-            className="w-full"
-            onClick={() => loginWithRedirect()}
-            state={"isActive"}
-          />
-        </IonFooter>
-      )}
+      <IonFooter className="p-4 mb-4">
+        <LargeDefaultButton
+          text="Continue"
+          className="w-full"
+          onClick={() => loginWithRedirect()}
+          state={"isActive"}
+        />
+      </IonFooter>
     </IonPage>
   );
 };
